dde7714415e2a40f98bdf62ebbbf84c075d16799
devherrera <cesarchavarria0@gmail.com>
2019-10-04 04:38:33 -0600

    App Configuration

--- start/kernel.js
+++ start/kernel.js
@@ -36,7 +36,8 @@ const globalMiddleware = [
 */
 const namedMiddleware = {
   auth: 'Adonis/Middleware/Auth',
-  guest: 'Adonis/Middleware/AllowGuestOnly'
+  guest: 'Adonis/Middleware/AllowGuestOnly',
+  owner: 'App/Middleware/OwnerDetector'
 }
 
 /*
@@ -51,7 +52,8 @@ const namedMiddleware = {
 */
 const serverMiddleware = [
   // 'Adonis/Middleware/Static',
-  'Adonis/Middleware/Cors'
+  'Adonis/Middleware/Cors',
+  // 'App/Middleware/OwnerDetector'
 ]
 
 Server
--- start/routes.js
+++ start/routes.js
@@ -19,3 +19,35 @@ const Route = use('Route')
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
+
+
+Route.group(() => {
+  Route.post('/register', 'AuthController.register')
+  Route.post('/login', 'AuthController.login')
+}).prefix('/auth')
+
+
+Route.group(() => {
+  // Categories
+  Route.get('/categories', 'CategoryController.index')
+  Route.get('/category/:categoryId', 'CategoryController.show')
+  Route.post('/category', 'CategoryController.store')
+  Route.put('/category/:categoryId', 'CategoryController.update')
+  Route.delete('/category/:categoryId', 'CategoryController.delete')
+
+  // Products
+  Route.get('/product/:productId', 'ProductController.show').middleware('owner')
+  Route.get('/products', 'ProductController.index')
+  Route.post('/product/', 'ProductController.store')
+  Route.put('/product/:productId', 'ProductController.update').middleware('owner')
+  Route.delete('/product/:productId', 'ProductController.delete').middleware('owner')
+
+  // Inventories
+  Route.get('/inventory/:inventoryId', 'InventoryController.show').middleware('owner')
+  Route.get('/inventories', 'InventoryController.index')
+  Route.post('/inventory', 'InventoryController.store')
+  // Assing Product to inventory
+  Route.post('/inventory/:inventoryId/product/:productId', 'InventoryController.addProduct').middleware('owner')
+
+
+}).prefix('/user').middleware('auth')
